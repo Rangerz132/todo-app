53e1312c5cb0cdb36509153d14eb8a27ca2af602
Anthony Trépanier <38358928+Rangerz132@users.noreply.github.com>
2025-02-13 14:20:14 -0500

    fix task

--- src/components/task/TaskItem.tsx
+++ src/components/task/TaskItem.tsx
@@ -1,66 +1,48 @@
-import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import CheckBox from "../CheckBox";
 import CrossIcon from "../../assets/images/icon-cross.svg";
 import { TaskType } from "../../types/taskType";
 import { TodoAPI } from "../../api/todo-api";
-import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../state/store";
 import { deleteTask, updateTask } from "../../state/task/taskSlice";
 
-const TaskItem = (props: { task: TaskType }) => {
-  const [isChecked, setIsChecked] = useState<boolean>(props.task.completed);
-  const currentTask = useSelector((store: RootState) =>
-    store.task.tasks.find((task) => task.id === props.task.id)
-  );
+const TaskItem = ({ task }: { task: TaskType }) => {
   const dispatch = useDispatch();
+  const currentTask = useSelector((store: RootState) =>
+    store.task.tasks.find((t) => t.id === task.id)
+  );
 
-  function handleCheck() {
-    setIsChecked((prevState) => {
-      const newCheckedState = !prevState;
-      setTask(newCheckedState);
-      return newCheckedState;
-    });
-  }
-  function handleCrossClick() {
-    removeTask();
-  }
+  const isChecked = currentTask?.completed ?? task.completed;
 
-  async function setTask(completed: boolean) {
-    const newTask: TaskType = {
-      task: props.task.task,
-      id: props.task.id,
-      completed,
-    };
-    const updatedTask = await TodoAPI.update(newTask);
+  const handleCheck = async () => {
+    const updatedTask = { ...task, completed: !isChecked };
+    await TodoAPI.update(updatedTask);
     dispatch(updateTask(updatedTask));
-  }
+  };
 
-  async function removeTask() {
-    await TodoAPI.deleteTaskById(props.task.id);
-    dispatch(deleteTask(currentTask));
-  }
+  const handleCrossClick = async () => {
+    await TodoAPI.deleteTaskById(task.id);
+    dispatch(deleteTask(task));
+  };
 
   return (
-    <div className="w-full  border-b border-neutral-light-theme-light-grayish-blue px-6 py-5 flex flex-row justify-between items-center dark:border-neutral-dark-theme-very-drak-grayish-blue-second">
+    <div className="w-full border-b border-neutral-light-theme-light-grayish-blue px-6 py-5 flex flex-row justify-between items-center dark:border-neutral-dark-theme-very-drak-grayish-blue-second">
       <div className="flex flex-row space-x-4">
         {/** Checkbox */}
-        <CheckBox onCheck={() => handleCheck()} isChecked={isChecked} />
+        <CheckBox onCheck={handleCheck} isChecked={isChecked} />
         {/** Task description */}
         <div
-          className={` ${
+          className={`transition-all ${
             isChecked
               ? "text-neutral-light-theme-light-grayish-blue line-through dark:text-neutral-dark-theme-very-dark-grayish-blue-main"
               : "text-neutral-light-theme-very-drak-grayish-blue dark:text-neutral-dark-theme-light-grayish-blue-main"
-          } transition-all`}
+          }`}
         >
-          {props.task.task}
+          {task.task}
         </div>
       </div>
       {/** Cross  */}
-      <div
-        onClick={() => handleCrossClick()}
-        className="cursor-pointer w-4 h-4"
-      >
+      <div onClick={handleCrossClick} className="cursor-pointer w-4 h-4">
         <img src={CrossIcon} alt="Cross icon" />
       </div>
     </div>
